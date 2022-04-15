034b678fd926199f40decd7f471dc96729d61a0a
Cengiz Bugra Aksakal <aksakalcengiz@hotmail.com>
2022-04-15 14:06:41 +0300

    commit tree f84cc85b13c981132fb2e1f0ccf98e816479ccea

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "Jane";
+
+changeLeastFavoriteCustomer();
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Mary";
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
